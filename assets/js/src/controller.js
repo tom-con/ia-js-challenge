2f7207323cf161718e5c1b9e48a72f62b8d10d73
Thomas Conchie <tconchie@gmail.com>
2018-05-11 12:08:49 -0600

    Adds some logic for setting initial state, for future reset button implementation

--- assets/js/src/controller.js
+++ assets/js/src/controller.js
@@ -1,6 +1,19 @@
 $(document).ready(() => {
 
     let dragging = false
+    let score = 0
+
+    function renderInitialState(){
+        $('.dot.in-position').hide()
+        $('.dot.in-menu').show()
+    }
+    // This series of methods controls the behavior
+    // of dots when they are clicked/dragged. This
+    // is needed to:
+    // 1. Ensure the correct dot is interacted with
+    // 2. That the user is actively dragging and not
+    //    clicking.
+    let dragging = false
     $('.dot')
         .mousedown(() => dragging = false )
         .mousemove((e) => {
