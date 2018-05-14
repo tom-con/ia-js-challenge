3650073533daae6c66d09514c70870aefed900df
Thomas Conchie <tconchie@gmail.com>
2018-05-13 22:55:49 -0600

    Sets up server to render the index.html page @ localhost:2000. Also sets up nodemon for dev and node for prod

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, '../client')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -32,7 +32,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.sendfile('error.html');
 });
 
 module.exports = app;
